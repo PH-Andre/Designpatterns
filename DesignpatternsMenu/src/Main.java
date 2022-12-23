5a071232213023cf227b5856fa9d7c9dfe836cb4
paulo H <paulohenrique.andre@gmail.com>
2022-12-23 20:44:41 -0300

    menu

---
+++ DesignpatternsMenu/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Lista de Designs");
+            System.out.println("1- Corrente of Responsability");
+            System.out.println("2- State");
+            System.out.println("3- Visitor");
+            System.out.println("4- Interpreter");
+            System.out.println("5- Composite");
+            System.out.println("6- Prototype");
+            System.out.println("7- Sair");
+            System.out.print("Digite o numero referente a sua escolha: ");
+            int opcao = scan.nextInt();
+
+
+
+
+    }
+}
